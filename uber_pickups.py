c6937bda69d5e2b7dfafbc3eeb245f48c9366400
Anna Kostenko <a.kostenko@a1qa.com>
2022-02-08 19:22:20 +0300

    DataFrame

--- uber_pickups.py
+++ uber_pickups.py
@@ -3,3 +3,9 @@ import pandas as pd
 import numpy as np
 
 st.title('Uber pickups in NYC')
+
+df = pd.DataFrame({
+  'first column': [1, 2, 3, 4],
+  'second column': [10, 20, 30, 40]
+})
+st.write(df)
