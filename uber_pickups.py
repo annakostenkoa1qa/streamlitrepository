20dac2a6a225231b48e5cb78bb965079aa207c57
Anna Kostenko <a.kostenko@a1qa.com>
2022-02-09 22:27:43 +0300

    streamlitapp

--- uber_pickups.py
+++ uber_pickups.py
@@ -9,3 +9,7 @@ df = pd.DataFrame({
   'second column': [10, 20, 30, 40]
 })
 st.write(df)
+
+import streamlit as st
+number = st.slider("Pick a number: ", min_value=1, max_value=10)
+st.text("Your number is " + str(number))
